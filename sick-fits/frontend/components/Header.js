45b8d672b5b7bc436b0346e7896b1b04112c3611
Jacob Meline <jacob.meline@gmail.com>
2018-10-24 20:13:54 -0600

    Fixing flashing when rendering. Getting ready for graphql

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Nav from './Nav';
 import styled from 'styled-components';
-import Link from 'next/link';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
